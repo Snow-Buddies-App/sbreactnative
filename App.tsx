5c4db6058f9490ce63f0ade4241ac55d336322a1
Todd Whitelow <duice352@gmail.com>
2023-09-05 08:35:23 -0600

    add welcome to snow buddies

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StyleSheet, Platform, StatusBar, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Welcome To Snow Buddies</Text>
+    </SafeAreaView>
+    
   );
 }
 
