e0f0ecbdc684fbcfc8570c38a245239609e54e9b
Joseph Slater McArdle <slater.mcardle@outlook.com>
2023-10-09 07:03:53 -0700

    compatibility updates

--- index.tsx
+++ index.tsx
@@ -1,19 +1,19 @@
-import 'expo-router/entry';
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
-import { Provider } from 'react-redux';
-import { PaperProvider } from 'react-native-paper';
-import store from './store';
-import rootTheme from './rootTheme';
+import "expo-router/entry";
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+import { Provider } from "react-redux";
+import { PaperProvider } from "react-native-paper";
+import store from "./store";
+import rootTheme from "./rootTheme";
 
 export default function App() {
   return (
-    <PaperProvider theme={rootTheme}>
-      <Provider store={store}>
+    <Provider store={store}>
+      <PaperProvider theme={rootTheme}>
         <SafeAreaView>
           <Text>Welcome To Snow Buddies</Text>
         </SafeAreaView>
-      </Provider>
-    </PaperProvider>
+      </PaperProvider>
+    </Provider>
   );
 }
