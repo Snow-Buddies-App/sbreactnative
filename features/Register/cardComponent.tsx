ee658c00223328756935bb457102c4b2445964e0
jaserino <jaserino1@gmail.com>
2023-11-16 14:45:15 -0500

    implemented fully functional carousel

--- features/Register/cardComponent.tsx
+++ features/Register/cardComponent.tsx
@@ -1,19 +1,48 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, ScrollView, Dimensions } from 'react-native';
+
+// TODO **
+// ADD functionality to pagination
 
 const CardComponent = () => {
+  const CARD_SPACING = 20;
+  const CARD_WIDTH = 315;
+  const ITEM_WIDTH = CARD_WIDTH + CARD_SPACING;
+  const data = [
+    {
+      title: 'This is the first card',
+    },
+    {
+      title: 'This is the second card',
+    },
+    {
+      title: 'This is the third card',
+    },
+  ];
   return (
-    <View style={styles.cardBox}>
-      <View style={styles.cardFlex}>
-        <Text style={styles.cardText}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        </Text>
-        <View style={styles.rectangleFlex}>
-          <View style={styles.rectanglePagination}></View>
-          <View style={styles.rectanglePagination}></View>
-          <View style={styles.rectanglePagination}></View>
-        </View>
-      </View>
-    </View>
+    <ScrollView
+      horizontal={true}
+      snapToInterval={ITEM_WIDTH}
+      decelerationRate={'fast'}
+      bounces={false}
+      showsHorizontalScrollIndicator={false}
+    >
+      {data.map((item, i) => {
+        return (
+          <View key={i} style={{ width: ITEM_WIDTH }}>
+            <View style={styles.cardBox}>
+              <View style={styles.cardFlex}>
+                <Text style={styles.cardText}>{item.title}</Text>
+                <View style={styles.rectangleFlex}>
+                  <View style={styles.rectanglePagination}></View>
+                  <View style={styles.rectanglePagination}></View>
+                  <View style={styles.rectanglePagination}></View>
+                </View>
+              </View>
+            </View>
+          </View>
+        );
+      })}
+    </ScrollView>
   );
 };
 
@@ -21,12 +50,12 @@ export default CardComponent;
 
 const styles = StyleSheet.create({
   cardBox: {
-    width: 315,
     height: 134,
     backgroundColor: 'rgba(144, 189, 225, .6)',
     borderRadius: 5,
     marginBottom: 15,
     justifyContent: 'center',
+    marginHorizontal: 8,
   },
   cardFlex: {
     alignItems: 'center',
